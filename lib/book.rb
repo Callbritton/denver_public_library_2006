9db1c40c433ffb25d5bb85728693355cec64caf3
Chris Allbritton <cla.inbox@gmail.com>
2020-07-22 15:02:43 -0400

    Update book.rb and book_test.rb to working through Iteration 1

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,12 @@
 class Book
+  attr_reader :author,
+              :title,
+              :publication_year
 
+  def initialize(info)
+    @author = info[:author_first_name] + " " +  info[:author_last_name]
+    @title = info[:title]
+    @publication_year = info[:publication_date].chars.last(4).join
+  end
 
-
-end 
+end
