957d98e071ec502971a48f78697a811040202711
Chris Allbritton <cla.inbox@gmail.com>
2020-07-22 14:34:12 -0400

    Update book.rb to include class Book and applicable test_it_exists

--- test/book_test.rb
+++ test/book_test.rb
@@ -4,9 +4,10 @@ require "./lib/book"
 
 class BookTest < Minitest::Test
 
-  #def test_it_exists
-    #assert_instance_of <Class>,
-  #end
+  def test_it_exists
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    assert_instance_of Book, book
+  end
 
   #def test_it_has_attributes
   #end
