0b99bae9c39299fd3309a271a77c5f8f7dd33534
Chris Allbritton <cla.inbox@gmail.com>
2020-07-22 16:06:36 -0400

    Update author attribute to string interpolation to avoid error around plus symbol

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,13 @@
 class Book
   attr_reader :author,
               :title,
-              :publication_year
+              :publication_year,
+              :publication_date
 
   def initialize(info)
-    @author = info[:author_first_name] + " " +  info[:author_last_name]
+    @author = "#{info[:author_first_name]} #{info[:author_last_name]}"
     @title = info[:title]
+    @publication_date = info[:publication_date]
     @publication_year = info[:publication_date].chars.last(4).join
   end
 
